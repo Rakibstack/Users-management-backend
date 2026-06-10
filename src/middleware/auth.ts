62001e4e30d9f84ca378a57dd34d9144f92b11fe
Rakibstack <rh6039511@gmail.com>
2026-06-10 18:15:58 +0600

    Validation Auth  Middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,50 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import config from "../config/env";
+import { pool } from "../db";
+
+const auth = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      res.status(401).json({
+        success: false,
+        message: "unauthorized access!",
+      });
+      return;
+    }
+    const decoded = jwt.verify(
+      token as string,
+      config.jwt_secret_key,
+    ) as JwtPayload;
+
+    const userData = await pool.query(
+      `
+       SELECT * FROM users WHERE email = $1 
+        `,
+      [decoded.email],
+    );
+
+    if (userData.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+      });
+      return;
+    }
+
+    const user = userData.rows[0];
+
+    if (!user.is_active) {
+      res.status(403).json({
+        success: false,
+        message: "Forbidden Access!",
+      });
+    }
+
+    next();
+  };
+};
+
+export default auth;
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,11 +1,12 @@
 import { Router, type Request, type Response } from "express";
 import userController from "./user.controller";
+import auth from "../../middleware/auth";
 
  const route = Router();
 
  route.post("/", userController.createUser);
 
- route.get("/",userController.getAllUser );
+ route.get("/",auth(),userController.getAllUser );
  route.get('/:id',userController.getSingleUser)
  route.put("/:id",userController.updateUser);
  route.delete("/:id",userController.deleteUser);
